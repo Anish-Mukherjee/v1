2c6d142bff98e39c2ed5e3aa72e3a8f17379aab4
Momin Kaleem <mominkaleem6@gmail.com>
2022-07-29 13:25:40 -0400

    Remove thirdweb

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,11 @@
-import { MoralisProvider } from "react-moralis";
 import "../styles/globals.css";
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <MoralisProvider
-      appId={process.env.NEXT_PUBLIC_APP_ID}
-      serverUrl={process.env.NEXT_PUBLIC_SERVER_URL}
-    >
-      <ChakraProvider>
+    <ChakraProvider>
       <Component {...pageProps} />
-      {console.log('Found Moralis')}
-      </ChakraProvider>
-    </MoralisProvider>
+    </ChakraProvider>
   );
 }
 
