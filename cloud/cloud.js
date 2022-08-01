43ca15cf35d3505ee565582ac6e7a9d8cd805b4b
Momin Kaleem <mominkaleem6@gmail.com>
2022-08-01 19:29:01 -0400

    Change query to Users

--- cloud/cloud.js
+++ cloud/cloud.js
@@ -1,6 +1,5 @@
 Moralis.Cloud.define("getExperts", async (request) => {
-  const query = new Moralis.Query("_EthAddress");
+  const query = new Moralis.Query("Users");
   const results = await query.find({ useMasterKey: true });
   return results;
 });
-
